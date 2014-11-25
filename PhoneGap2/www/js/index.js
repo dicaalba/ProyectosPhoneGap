ee882d5cce88e0ff8580f9a609e983a3dbf45047
dicaalba <dicaalba@gmail.com>
2014-11-25 15:22:36 -0430

    Camara

---
+++ PhoneGap2/www/js/index.js
@@ -0,0 +1,41 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+};
+
+window.onload=function(){
+    document.getElementById("foto").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("foto").style.height = (window.innerWidth-50)+"px";
+    document.getElementById("foto").style.backgroundSize="50% 50%";
+};
+
+function capturePhoto() {
+    navigator.camera.getPicture(onSuccess, onFail, { quality: 90,
+        destinationType: Camera.DestinationType.DATA_URL,
+        correctOrientation: true,
+        targetWidth: 1000,
+        targetHeight: 1000
+    });
+}
+
+function onSuccess(imageData) {
+    document.getElementById("foto").style.backgroundImage="url('data:image/jpeg;base64,"+imageData+"')";
+    document.getElementById("foto").style.backgroundSize="100% 100%";
+}
+
+function onPhotoURISuccess(imageURI) {
+    document.getElementById("foto").style.backgroundImage="url('"+imageURI+"')";
+    document.getElementById("foto").style.backgroundSize="100% 100%";
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
